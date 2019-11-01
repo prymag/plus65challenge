9dde0a7371a8e2c5fbc7a96429893c2e2e6ca73d
Perry <perry@accordia.com>
2019-11-01 17:03:30 +0800

    seeds

---
+++ database/seeds/WinningNumbersSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\WinningNumber;
+use App\Services\MemberService;
+use Illuminate\Database\Seeder;
+
+class WinningNumbersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $member_service = App::make(MemberService::class);
+
+        $members = $member_service->getAll();
+        
+        $members->each(function($member) {
+            factory(WinningNumber::class, rand(1,10))->create([
+                'member_id' => $member->id
+            ]);
+        });
+
+    }
+}
