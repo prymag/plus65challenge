4501c4506ceb79c07644d6abdbc0958bffd76421
perry <User@Tipzee-Aspire.(none)>
2019-11-02 17:25:24 +0800

    Prize

---
+++ app/Modules/Prize/CollectionParser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Modules\Prize;
+
+use Illuminate\Support\Collection;
+
+class CollectionParser {
+
+    /**
+     * Group Relevant Prizes in array
+     *
+     * @param Collection $collection
+     * @return Array
+     */
+    public function groupPrizes(Collection $collection)
+    {
+        # code...
+        return $collection->reduce(function ($arr, $item) {
+            //
+            $key = preg_replace('/_[0-9]/', '', $item->key);
+            $key = str_replace('_', '-', $key);
+
+            $arr[$key][] = $item;
+            return $arr;
+            //return $carry + $item;
+        }, []);
+    }
+
+}
