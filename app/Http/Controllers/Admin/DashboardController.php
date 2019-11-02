1ab65dfc6b07985cda19358c8984a03be97e5a77
perry <User@Tipzee-Aspire.(none)>
2019-11-02 17:27:25 +0800

    Prize board display

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,22 +3,35 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Modules\Prize\CollectionParser as PrizeCollectionParser;
+use App\Modules\Prize\PrizeService;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     //
-    public function __construct()
-    {
+    protected $prize_service;
+
+    protected $prize_collection_parser;
+    //
+    public function __construct(
+        PrizeService $prize_service,
+        PrizeCollectionParser $prize_collection_parser
+    ) {
         # code...
+        $this->prize_service = $prize_service;
+        $this->prize_collection_parser = $prize_collection_parser;
     }
 
     public function index()
     {
         # code...
-        return view('admin.dashboard.index');
+        $prizes = $this->prize_service->getAll();
+        $prizes_grouped = $this->prize_collection_parser->groupPrizes($prizes);
+
+        $data = compact('prizes', 'prizes_grouped');
+        
+        return view('admin.dashboard.index', $data);
     }
-
-
 
 }
