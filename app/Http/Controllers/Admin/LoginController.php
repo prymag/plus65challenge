e67f364aa4ea9dcd3f835f15aa7e1d2167f12c2d
perry <User@Tipzee-Aspire.(none)>
2019-11-02 17:27:59 +0800

    change redirect

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
 {
     use AuthenticatesUsers;
 
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin';
     //
     public function __construct()
     {
