3717b87eeff5215960274e7c26295cfc335e207a
perry <User@Tipzee-Aspire.(none)>
2019-11-03 11:58:07 +0800

    refactor

--- resources/views/admin/dashboard/components/clear-btn.blade.php
+++ resources/views/admin/dashboard/components/clear-btn.blade.php
@@ -1,9 +1,10 @@
-@foreach($prizes as $prize)
-    @if ($prize->winner)
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="d-flex justify-content-end align-items-center">
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="d-flex justify-content-end align-items-center">
+                @foreach($prizes as $prize)
+                    @if ($prize->winner)
                         {{ Form::open(
                             [
                                 'url' => route('admin.clear-winners'), 
@@ -13,27 +14,28 @@
                             <button class="btn btn-primary"> {{ __('Clear Winners') }}</button>
                         {{ Form::close() }}
                         <span class="mx-3">{{ __('Or')}} </span>
-                        {{ Form::open(
-                            [
-                                'url' => route('admin.re-seed'), 
-                                'method' => 'post'
-                            ]
-                        ) }}
-                            <button class="btn btn-primary"> {{ __('Re-Seed') }}</button>
-                        {{ Form::close() }}
-                        <span class="mx-5">|</span>
-                        {{ Form::open(
-                            [
-                                'url' => route('admin.logout'), 
-                                'method' => 'post'
-                            ]
-                        ) }}
-                            <button class="btn btn-primary"> {{ __('Logout') }}</button>
-                        {{ Form::close() }}
-                    </div>
-                </div>
+                    @break
+                    @endif
+                @endforeach
+                {{ Form::open(
+                    [
+                        'url' => route('admin.re-seed'), 
+                        'method' => 'post'
+                    ]
+                ) }}
+                    <button class="btn btn-primary"> {{ __('Re-Seed') }}</button>
+                {{ Form::close() }}
+                <span class="mx-5">|</span>
+                {{ Form::open(
+                    [
+                        'url' => route('admin.logout'), 
+                        'method' => 'post'
+                    ]
+                ) }}
+                    <button class="btn btn-primary"> {{ __('Logout') }}</button>
+                {{ Form::close() }}
             </div>
         </div>
-        @break
-    @endif
-@endforeach
+    </div>
+</div>
+        
