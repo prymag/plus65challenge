ad7058e5a148164cb295ca322f7bf910b5c4f07c
perry <User@Tipzee-Aspire.(none)>
2019-11-03 09:52:36 +0800

    Default values

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,31 +5,56 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Modules\Prize\CollectionParser as PrizeCollectionParser;
 use App\Modules\Prize\PrizeService;
+use App\Modules\Users\Member\MemberService;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     //
+    protected $member_service;
+
     protected $prize_service;
 
     protected $prize_collection_parser;
     //
     public function __construct(
+        MemberService $member_service,
         PrizeService $prize_service,
         PrizeCollectionParser $prize_collection_parser
     ) {
         # code...
+        $this->member_service = $member_service;
         $this->prize_service = $prize_service;
         $this->prize_collection_parser = $prize_collection_parser;
     }
 
-    public function index()
+    public function index(Request $request)
     {
         # code...
         $prizes = $this->prize_service->getAll();
+        $prizes_options = $this->prize_collection_parser->toSelectOpts($prizes);
         $prizes_grouped = $this->prize_collection_parser->groupPrizes($prizes);
+       
+        $winning_number = $request->session()->get('winning_number');
+        
+        $prize_id = old('prize_id') ?
+            old('prize_id') : 
+            $request->session()->get('prize_id');
+        $randomized = old('randomized') ?
+            old('randomized') :
+            $request->session()->get('randomized');
 
-        $data = compact('prizes', 'prizes_grouped');
+        $crosstab = $this->member_service->memberWinningNumberCrosstab();
+        
+        $data = compact(
+            'prizes', 
+            'prizes_grouped', 
+            'prizes_options', 
+            'winning_number', 
+            'crosstab',
+            'prize_id',
+            'randomized'
+        );
         
         return view('admin.dashboard.index', $data);
     }
