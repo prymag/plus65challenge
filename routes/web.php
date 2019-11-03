08b63ce32f952b258c9ea7ef70b80f381c415e01
perry <User@Tipzee-Aspire.(none)>
2019-11-03 11:57:39 +0800

    added middleware + member route

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,16 @@ Route::post('/admin/logout', 'Admin\LoginController@logout')->name('admin.logout
 Route::post('/admin/login', 'Admin\LoginController@login')->name('admin.do.login');
 
 
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin', 'middleware' => 'auth.admin'], function() {
     Route::get('/', 'Admin\DashboardController@index')->name('admin.dashboard');
     Route::post('/generate-prizes', 'Admin\DrawGeneratorController@generatePrizes')->name('admin.generate-prizes');
     Route::post('/generate-prize', 'Admin\DrawGeneratorController@generatePrize')->name('admin.generate-prize');
     Route::post('/clear-winners', 'Admin\WinnersController@clearWinners')->name('admin.clear-winners');
     Route::post('/re-seed', 'Admin\ReseederController@reSeed')->name('admin.re-seed');
+});
+
+Route::group(['prefix' => 'member'], function() {
+    Route::get('/', 'Member\MemberController@index')->name('member.index');
 });
 
 Route::group(['prefix' => 'ajax'], function() {
